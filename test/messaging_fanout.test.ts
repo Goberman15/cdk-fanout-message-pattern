78a8c500ba98130016af048ee4b1df7bd887c9d7
goberman15 <akbarrmdhn94@gmail.com>
2021-12-13 12:35:09 +0700

    Initial commit

---
+++ test/messaging_fanout.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MessagingFanout from '../lib/messaging_fanout-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/messaging_fanout-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MessagingFanout.MessagingFanoutStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
