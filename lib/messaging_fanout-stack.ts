7f188e4fea6a84101e2d234bb5b26f8ee135ac67
goberman15 <akbarrmdhn94@gmail.com>
2021-12-21 17:20:27 +0700

    simple fanout messaging pattern

--- lib/messaging_fanout-stack.ts
+++ lib/messaging_fanout-stack.ts
@@ -1,16 +1,31 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions'
 
 export class MessagingFanoutStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue1 = new sqs.Queue(this, 'fanout-queue-1', {
+      queueName: 'fanout-queue-1'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MessagingFanoutQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue2 = new sqs.Queue(this, 'fanout-queue-2', {
+      queueName: 'fanout-queue-2'
+    });
+
+    const topic = new sns.Topic(this, 'fanout-topic', {
+      topicName: 'fanout-topic',
+      displayName: 'fanout-topic'
+    });
+
+    topic.addSubscription(new subs.SqsSubscription(queue1));
+    topic.addSubscription(new subs.SqsSubscription(queue2));
+
+    new CfnOutput(this, 'topicArn', {
+      value: topic.topicArn
+    });
   }
 }
